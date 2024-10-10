45cbdca872493fbe62ac54eff501e03f8a1940b7
Vladyslav Paun <paunvlad122@gmail.com>
2024-10-10 17:49:50 +0200

    Initial commit

---
+++ src/main/java/com/project/wab/domain/Product.java
@@ -0,0 +1,9 @@
+package com.project.wab.domain;
+
+/**
+ * @author "Vladyslav Paun"
+ */
+public class Product {
+    
+    private String name;
+}
---
+++ src/test/java/com/project/wab/WabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.wab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
