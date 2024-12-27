683e4ce013fa4af3da026c642543630e9e9f7ee0
Vladyslav Paun <paunvlad122@gmail.com>
2024-12-27 18:42:13 +0100

    Implement show product for cart list and calculate total price

--- src/main/java/com/project/wab/controller/cart/CartListController.java
+++ src/main/java/com/project/wab/controller/cart/CartListController.java
@@ -1,10 +1,18 @@
 package com.project.wab.controller.cart;
 
-import com.project.wab.service.CartService;
+import com.project.wab.dto.CartItemDTO;
+import com.project.wab.service.CartItemService;
+import com.project.wab.utils.WebUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @author "Vladyslav Paun"
@@ -13,11 +21,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/cart")
 public class CartListController {
-    private final CartService cartService;
 
-    @GetMapping("/list")
-    public String showCart() {
+    private final CartItemService cartItemService;
+// Check is token (if token is blank - return cart/list)
+// if ok - get products and return cart/list
+// if bad token return cart/list
+@GetMapping("/list")
+public String showCart(Model model, HttpServletRequest request) {
+    String cartToken = WebUtil.checkToken(request);
+
+    if (cartToken == null || cartToken.isBlank()) {
+        model.addAttribute("cartItems", List.of());
+        model.addAttribute("totalPrice", BigDecimal.ZERO);
         return "cart/list";
     }
+
+    try {
+        UUID cartId = UUID.fromString(cartToken);
+        List<CartItemDTO> cartItems = cartItemService.getCartItemsDTOByCartId(cartId);
+        BigDecimal totalPrice = cartItemService.calculateTotalPriceByCartId(cartId);
+
+        model.addAttribute("cartItems", cartItems);
+        model.addAttribute("totalPrice", totalPrice);
+        return "cart/list";
+
+    } catch (IllegalArgumentException e) {
+        model.addAttribute("cartItems", List.of());
+        model.addAttribute("totalPrice", BigDecimal.ZERO);
+        return "cart/list";
+    }
+}
+
 }
 
---
+++ src/main/java/com/project/wab/dto/CartItemDTO.java
@@ -0,0 +1,20 @@
+package com.project.wab.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * @author "Vladyslav Paun"
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItemDTO {
+    private String productName;
+    private BigDecimal price;
+    private int quantity;
+    private BigDecimal total;
+}
