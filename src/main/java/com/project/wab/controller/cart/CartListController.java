9c4ce79e19b9d87d3d7bfccd4ac9c939335742a3
Vladyslav Paun <paunvlad122@gmail.com>
2024-12-27 16:05:58 +0100

    Add logics for cart list with static product

--- src/main/java/com/project/wab/controller/cart/CartListController.java
+++ src/main/java/com/project/wab/controller/cart/CartListController.java
@@ -1,19 +1,10 @@
 package com.project.wab.controller.cart;
 
-import com.project.wab.domain.Cart;
-import com.project.wab.domain.CartItem;
 import com.project.wab.service.CartService;
-import com.project.wab.utils.WebUtil;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author "Vladyslav Paun"
@@ -24,15 +15,9 @@ import java.util.List;
 public class CartListController {
     private final CartService cartService;
 
-//    @GetMapping("/list")
-//    public String list(Model model, HttpServletRequest request) {
-//        String token = WebUtil.checkToken(request);
-//
-//        Cart cart = cartService.getCartByToken(token);
-//        List<CartItem> cartItems = cart.getItems();
-//
-//        model.addAttribute("cartItems", cartItems);
-//        return "cart/list";
-//    }
+    @GetMapping("/list")
+    public String showCart() {
+        return "cart/list";
+    }
 }
 
